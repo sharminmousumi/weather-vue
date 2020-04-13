d0cdfb3dfa19f3744864671b8cd150f2f70317e2
jahirul <[haqjahirul1983@gmail.com]>
2020-04-13 14:06:24 +0200

    add city

---
+++ src/components/addNewCity.vue
@@ -0,0 +1,58 @@
+<template >
+<div  >
+     <form >
+        <input  type="text" v-model ="newCity">
+        
+        <button  type="button" @click="addCity" class="button">Add City</button>
+    </form> 
+    
+    <div  >
+
+    <ul>
+        <li v-for="(city,index) in citys"   v-bind:key="city" >
+
+            {{city}} <button  type="button" @click="deleteCity(index)"> x </button>  
+        </li>
+        
+     </ul>
+    </div>
+    
+
+</div>
+</template>
+
+
+<script>
+
+export default {
+  
+    data() {
+        return {
+        
+       citys:['Goteborg','London','Paris'],
+       newCity:''
+       }
+        
+    },
+    methods: {
+          addCity(){
+           
+            this.citys.push(this.newCity);
+             this.newCity='' ;    
+
+        },
+
+        deleteCity:function(index){
+            this.citys.splice(index,1);
+
+        }
+       
+    }
+}
+</script>
+<style scoped>
+li{
+    list-style: none;
+
+}
+</style>
